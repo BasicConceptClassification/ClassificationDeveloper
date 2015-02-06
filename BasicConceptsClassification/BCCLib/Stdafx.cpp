2345a1f426591f81f74be59fd97e2ddc525e6cf8
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-06 12:29:43 -0700

    Restructured project and added templates

---
+++ BasicConceptsClassification/BCCLib/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// BCCLib.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
--- BasicConceptsClassification/BasicConceptsClassification/BasicConceptsClassification.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-using namespace BasicConceptsClassificationLib;
-
-int main(int argc, char *argv[])
-{
-	printf( "Hello world! %d\n", Example::foo(0) );
-    return 0;
-}
