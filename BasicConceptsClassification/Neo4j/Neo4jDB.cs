d36fa36a17f659fab47b294ae684d6f9b8cedabd
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-10 13:24:56 -0700

    Project is linked up now. Everything is good.

--- BasicConceptsClassification/Neo4j/Neo4jDB.cs
+++ BasicConceptsClassification/Neo4j/Neo4jDB.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using BCCLib;
+
 namespace Neo4j
 {
     class Neo4jDB
     {
+        public ClassifiableCollection query(ConceptString cstring)
+        {
+            return null;
+        }
     }
 }
