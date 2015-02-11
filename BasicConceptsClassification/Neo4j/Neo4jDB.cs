fa0affd0740479d8939684fd3816fc8358073b11
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-11 01:33:09 -0700

    Added Neo4j Client lib

--- BasicConceptsClassification/Neo4j/Neo4jDB.cs
+++ BasicConceptsClassification/Neo4j/Neo4jDB.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 
 using BCCLib;
+using Neo4jClient;
+using Neo4jClient.Cypher;
 
 namespace Neo4j
 {
     class Neo4jDB
     {
+        protected Uri dbLocation = new Uri("http://localhost:7474/db/data");
+
+        public void open()
+        {
+            
+        }
+
         public ClassifiableCollection query(ConceptString cstring)
         {
             return null;
---
+++ BasicConceptsClassification/Neo4jTest/Neo4jDBTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neo4jTest
+{
+    [TestClass]
+    public class Neo4jDBTest
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCSQuery()
+        {
+
+        }
+    }
+}
