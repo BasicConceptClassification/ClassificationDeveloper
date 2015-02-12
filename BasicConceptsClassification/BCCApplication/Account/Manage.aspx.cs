31c7383c2e5a3f227d75bd3a5b0435f07a4c2cdd
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-12 12:00:11 -0700

    Fixed some weird garbage that happened at the merge.

--- BasicConceptsClassification/BCCApplication/Account/Manage.aspx.cs
+++ BasicConceptsClassification/BCCApplication/Account/Manage.aspx.cs
@@ -7,10 +7,8 @@ using System.Web.UI.WebControls;
 using Microsoft.AspNet.Membership.OpenAuth;
 
 namespace BCCApplication.Account
-public partial class Account_Manage : System.Web.UI.Page
 {
     public partial class Manage : System.Web.UI.Page
-    protected void Page_Load(object sender, EventArgs e)
     {
         protected string SuccessMessage
         {
--- BasicConceptsClassification/BCCApplication/Account/Register.aspx.designer.cs
+++ BasicConceptsClassification/BCCApplication/Account/Register.aspx.designer.cs
@@ -29,14 +29,5 @@ namespace BCCApplication.Account {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.CreateUserWizardStep RegisterUserWizardStep;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
