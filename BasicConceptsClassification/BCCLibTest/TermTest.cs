fd8e6fb887d1d8b25c54cdfd93a74e1494d7f0b6
Bronte Lee <bronte@ualberta.ca>
2015-02-19 13:18:49 -0700

    Some tests for the Term class.

---
+++ BasicConceptsClassification/BCCLibTest/TermTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BCCLib;
+using System.Collections.Generic;
+
+namespace BCCLibTest
+{
+    [TestClass]
+    public class TermTest
+    {
+        [TestMethod]
+        public void TestHasSubTerm_Found()
+        {
+            Term t = new Term
+            {
+                id = "id01",
+                rawTerm = "Raw",
+                subTerms = new List<Term>(),
+            };
+
+            Term subT = new Term
+            {
+                id = "id02",
+                rawTerm = "subRaw",
+                subTerms = new List<Term>(),
+            };
+
+            t.subTerms.Add(subT);
+
+            int index = t.hasSubTerm(subT);
+
+            Assert.AreEqual(0, index);
+            Assert.AreEqual(subT, t.subTerms[index]);
+        }
+
+        public void TestHasSubTerm_NotFound()
+        {
+            Term t = new Term
+            {
+                id = "id01",
+                rawTerm = "Raw",
+                subTerms = new List<Term>(),
+            };
+
+            Term subT = new Term
+            {
+                id = "id02",
+                rawTerm = "subRaw",
+                subTerms = new List<Term>(),
+            };
+
+            Term t2 = new Term
+            {
+                id = "id03",
+                rawTerm = "Raw2",
+                subTerms = new List<Term>(),
+            };
+
+            t.subTerms.Add(subT);
+
+            int index = t.hasSubTerm(t2);
+
+            Assert.AreEqual(-1, index);
+        }
+
+        public void TestHasSubTerm_NoSubTerms()
+        {
+            Term t = new Term
+            {
+                id = "id01",
+                rawTerm = "Raw",
+                subTerms = new List<Term>(),
+            };
+
+            Term subT = new Term
+            {
+                id = "id02",
+                rawTerm = "subRaw",
+                subTerms = new List<Term>(),
+            };
+
+            int index = t.hasSubTerm(subT);
+
+            Assert.AreEqual(-1, index);
+        }
+
+        public void TestConnectTermsFromList() 
+        {
+            Term t = new Term
+            {
+                id = "id01",
+                rawTerm = "Raw",
+                subTerms = new List<Term>(),
+            };
+
+            Term subT = new Term
+            {
+                id = "id02",
+                rawTerm = "subRaw",
+                subTerms = new List<Term>(),
+            };
+
+            Term subT2 = new Term
+            {
+                id = "id03",
+                rawTerm = "subRaw3",
+                subTerms = new List<Term>(),
+            };
+
+            List<Term> termList = new List<Term> 
+            {
+                subT, subT2,
+            };
+
+            t.connectTermsFromList(termList);
+
+            // Expect the following structure:
+            // t.subTerms has subT, subT.subTerms has subT2 
+            Assert.AreEqual(1, t.subTerms.Count);
+            Assert.AreEqual(subT, t.subTerms[0]);
+            Assert.AreEqual(1, t.subTerms[0].subTerms.Count);
+            Assert.AreEqual(subT2, t.subTerms[0].subTerms[0]);
+            Assert.AreEqual(0, t.subTerms[0].subTerms[0].subTerms.Count);
+        }
+    }
+}
