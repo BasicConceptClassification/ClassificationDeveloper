91af9a70bff09c7bcfcdd834dacf57c59af44c0c
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-25 08:13:27 -0700

    Added some login routines

--- BasicConceptsClassification/BCCApplication/Account/Login.aspx.cs
+++ BasicConceptsClassification/BCCApplication/Account/Login.aspx.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
 
 namespace BCCApplication.Account
 {
@@ -18,6 +18,34 @@ namespace BCCApplication.Account
             if (!String.IsNullOrEmpty(returnUrl))
             {
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Validate the user password
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+                // This doen't count login failures towards account lockout
+                // To enable password failures to trigger lockout, change to shouldLockout: true
+                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: true);
+
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        break;
+                    case SignInStatus.LockedOut:
+                        Response.Redirect("/Account/Lockout");
+                        break;
+                    case SignInStatus.RequiresVerification:
+                        break;
+                    case SignInStatus.Failure:
+                    default:
+                        break;
+                }
             }
         }
     }
