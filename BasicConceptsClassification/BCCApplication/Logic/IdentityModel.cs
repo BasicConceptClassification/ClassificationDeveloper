f772e1947c4e576434d724c49a156d6569497fea
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-27 02:27:12 -0700

    omfg why did that work

--- BasicConceptsClassification/BCCApplication/Logic/IdentityModel.cs
+++ BasicConceptsClassification/BCCApplication/Logic/IdentityModel.cs
@@ -30,7 +30,7 @@ namespace BCCApplication.Logic
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
         }
 
