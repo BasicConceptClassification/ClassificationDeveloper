8a7bb9733e39dbd7de51d3a681d0c42056fdf849
Kim Kramer <kkramer1@ualberta.ca>
2015-03-03 09:36:25 -0700

    Classifying a GLAM object page... pretty much done

---
+++ BasicConceptsClassification/BCCApplication/Account/ClassOb.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BCCApplication.Account
+{
+    public partial class ClassOb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
