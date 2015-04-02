0ef8b9e94889229828c52bde76d76fcf38abe304
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-04-02 11:26:46 -0600

    Was working on the all users view, but that kinda mostly doesn't work so you know....

---
+++ BasicConceptsClassification/BCCApplication/Account/AdminViewAllUsers.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminViewAllUsers.aspx.cs" Inherits="BCCApplication.Account.AdminViewAllUsers" %>
+<asp:Content ID="Content" ContentPlaceHolderID="MainContent" runat="server">
+
+<div>
+    <asp:GridView ID="GridView" runat="server">
+    </asp:GridView>
+</div>
+
+</asp:Content>
---
+++ BasicConceptsClassification/BCCApplication/Account/AdminViewAllUsers.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
+using System.Web.Security;
+
+namespace BCCApplication.Account
+{
+    public partial class AdminViewAllUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int numRecords;
+            MembershipUserCollection users = Membership.GetAllUsers(0, 100, out numRecords);
+
+            GridView.DataSource = users;
+            GridView.DataBind();
+        }
+    }
+}
